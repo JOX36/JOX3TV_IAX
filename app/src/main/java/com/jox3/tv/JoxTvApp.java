76e10b16721829f79d2f9dfcbc986dbf4db24eda
JOX36 <angelvalenciamurcielago@gmail.com>
2026-06-20 10:05:45 -0500

    Add crash logging in JoxTvApp
    Implement custom uncaught exception handler to log crashes.

--- app/src/main/java/com/jox3/tv/JoxTvApp.java
+++ app/src/main/java/com/jox3/tv/JoxTvApp.java
@@ -1,10 +1,30 @@
 package com.jox3.tv;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.jox3.tv.util.AppPrefs;
 
 public class JoxTvApp extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        final Thread.UncaughtExceptionHandler defaultHandler =
+                Thread.getDefaultUncaughtExceptionHandler();
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            try {
+                String trace = Log.getStackTraceString(throwable);
+                new AppPrefs(getApplicationContext()).saveCrashLog(trace);
+            } catch (Exception ignored) {
+            }
+            if (defaultHandler != null) {
+                defaultHandler.uncaughtException(thread, throwable);
+            } else {
+                System.exit(1);
+            }
+        });
     }
 }
