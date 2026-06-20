f16f499f4a961fec86d92a4e263a87fa4dd44aa2
JOX36 <angelvalenciamurcielago@gmail.com>
2026-06-19 23:27:49 -0500

    Create HomeActivity for the home screen

---
+++ app/src/main/java/com/jox3/tv/ui/home/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.jox3.tv.ui.home;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.jox3.tv.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
